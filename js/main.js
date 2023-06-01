d4eb200f8c12dbd995eb2e826664955eeb06b808
Husniddin Abdullayev <132268896+Hussi123@users.noreply.github.com>
2023-06-01 17:17:13 +0500

    header end

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const input=document.querySelector(".search-input");
+
+console.log(input.value);
+
+input.onkeyup= (element) =>{
+    if(element.code==="Enter"){
+        let url='https://www.google.com/search?q='+input.value;
+        window.open(url, "_self");
+        tozala()
+        alert("ok")
+    }
+}
+
+function tozala(){
+    input.value="";
+}
